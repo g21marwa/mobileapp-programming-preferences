870621dbf065c10b71ab0878099875b9c16a688a
Martin Warg <g21marwa@student.his.se>
2022-05-05 09:15:08 +0200

    Added a onPause function in main to reset the textview

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -11,7 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
+    private TextView tv;
+    private SharedPreferences myShared;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +32,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        SharedPreferences myShared = getSharedPreferences(String.valueOf(R.string.myPreferences), Context.MODE_PRIVATE);
+        myShared = getSharedPreferences(String.valueOf(R.string.myPreferences), Context.MODE_PRIVATE);
         String myText = myShared.getString(String.valueOf(R.string.mySharedText), "still nothing");
         tv.setText(myText);
     }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = myShared.edit();
+        editor.clear();
+        editor.apply();
+        Log.d("asd", "asd");
+        tv.setText("Nothing again");
+    }
+
 }
