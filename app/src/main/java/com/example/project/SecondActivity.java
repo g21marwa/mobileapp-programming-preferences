001815a358413bf3583444884e08b48d82dae4bf
Martin Warg <g21marwa@student.his.se>
2022-05-05 07:59:01 +0200

    fixed some warnings

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -15,7 +12,6 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        TextView tv = findViewById(R.id.isWorkingText);
         SharedPreferences myShared = this.getSharedPreferences(String.valueOf(R.string.myPreferences), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = myShared.edit();
         editor.putString(String.valueOf(R.string.mySharedText), "it's working");
