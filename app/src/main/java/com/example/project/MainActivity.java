a2d14d9c69627c3d701ec2d144410fab66bb8561
Martin Warg <g21marwa@student.his.se>
2022-05-05 07:53:22 +0200

    Added a second activity that can be started from maniActivity. SecondActivity sets a shared preference string and MainActivity gets it.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.project;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        tv = findViewById(R.id.sharedText);
+
+    }
+    public void startActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences myShared = getSharedPreferences("myPreference", Context.MODE_PRIVATE);
+        String myText = myShared.getString("mySharedText", "still nothing");
+        tv.setText(myText);
+    }
 }
---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView tv = findViewById(R.id.isWorkingText);
+        SharedPreferences myShared = this.getSharedPreferences("myPreference", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = myShared.edit();
+        editor.putString("mySharedText", "it's working");
+        editor.apply();
+    }
+}
