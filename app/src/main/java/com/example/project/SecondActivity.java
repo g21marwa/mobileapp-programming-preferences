bd764db7ba47285cb79692931e0de54f54974a51
Martin Warg <g21marwa@student.his.se>
2022-05-06 09:27:17 +0200

    Added an edittext and a button so you can dynamically set the preference value

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,9 +14,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+    }
+
+    public void setPreference(View view) {
+        EditText et = findViewById(R.id.nameInput);
+        String name = et.getText().toString();
         SharedPreferences myShared = this.getSharedPreferences(String.valueOf(R.string.myPreferences), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = myShared.edit();
-        editor.putString(String.valueOf(R.string.mySharedText), "it's working");
+        editor.putString(String.valueOf(R.string.mySharedText), name);
         editor.apply();
     }
 }
